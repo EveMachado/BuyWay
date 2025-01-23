1f79440e5939c4380b93c7291c1723e1f096d1cf
Evellyn Machado <fernandaevellyn337@gmail.com>
2025-01-22 23:42:51 -0300

    métodos subTotal e total

--- src/main/java/com/gestaoseries/poo/entities/Order.java
+++ src/main/java/com/gestaoseries/poo/entities/Order.java
@@ -45,6 +45,7 @@ public class Order implements Serializable {
 	
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
+	
 
 	public Order() {
 
@@ -106,6 +107,14 @@ public class Order implements Serializable {
 		this.payment = payment;
 	}
 
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x : items) {
+			sum += x.getsubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
